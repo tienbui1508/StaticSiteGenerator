0049f6a80561c6011c9024ed96722f973a102e35
Tien Bui <tienbui1508@gmail.com>
2026-07-04 21:42:36 +0930

    add text to textnodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -82,3 +82,11 @@ def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
 
+def text_to_textnodes(text:str) -> list[TextNode]:
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -7,6 +7,7 @@ from inline_markdown import (
     extract_markdown_links,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 
 
@@ -196,23 +197,41 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
-    # def test_split_links(self):
-    #     node = TextNode(
-    #         "This is text with a [link](https://boot.dev) and [another link](https://wikipedia.org) with text that follows",
-    #         TextType.TEXT,
-    #     )
-    #     new_nodes = split_nodes_link([node])
-    #     self.assertListEqual(
-    #         [
-    #             TextNode("This is text with a ", TextType.TEXT),
-    #             TextNode("link", TextType.LINK, "https://boot.dev"),
-    #             TextNode(" and ", TextType.TEXT),
-    #             TextNode("another link", TextType.LINK, "https://wikipedia.org"),
-    #             TextNode(" with text that follows", TextType.TEXT),
-    #         ],
-    #         new_nodes,
-    #     )
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a [link](https://boot.dev) and [another link](https://wikipedia.org) with text that follows",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("another link", TextType.LINK, "https://wikipedia.org"),
+                TextNode(" with text that follows", TextType.TEXT),
+            ],
+            new_nodes,
+        )
 
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
+        )
 
 if __name__ == "__main__":
     unittest.main()
