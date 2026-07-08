13fc071535199badf87c7c2294bc582e4a200862
Tien Bui <tienbui1508@gmail.com>
2026-07-09 03:28:27 +0930

    add generate all pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -25,3 +25,17 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as file:
         file.write(template)
     print(f"Page generated successfully at {dest_path}")
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename).replace(".md", ".html")
+
+        if os.path.isfile(from_path) and filename.endswith(".md"):
+            dest_path = os.path.join(dest_dir_path, filename).replace(".md", ".html")
+            generate_page(from_path, template_path, dest_path)
+        elif os.path.isdir(from_path):
+            generate_pages_recursive(from_path, template_path, dest_path)
